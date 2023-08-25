4eca0d83e04fda8db4ca9512824690c4d1a0a78f
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-08-25 09:44:28 -0300

    template driven

---
+++ template-driven/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateDrivenFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
